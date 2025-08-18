17e4bbe698163e87b7af6e07e7ce524be796fe3d
BikitSuwal <bikitsuwal1@gmail.com>
2025-08-18 22:55:13 +0545

    Time: 3 ms (52.83%) | Memory: 12.4 MB (99.04%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')':'(','}':'{',']':'['}
+        
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack.pop()!= mapping[char]:
+                    return False
+                    break
+        else:
+            return len(stack)==0
+                    
+    
